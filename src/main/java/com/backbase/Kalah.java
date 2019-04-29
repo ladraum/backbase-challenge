6986d8428d64a91246b7b1cc75d745cc87816ca6
Ricardo MB <ladraum@gmail.com>
2019-04-29 01:21:48 -0700

    Added base class with sample test

---
+++ src/main/java/com/backbase/Kalah.java
@@ -0,0 +1,8 @@
+package com.backbase;
+
+import lombok.Data;
+
+@Data
+public class Kalah {
+    private Integer gameNumber;
+}
---
+++ src/test/java/com/backbase/KalahTest.java
@@ -0,0 +1,13 @@
+package com.backbase;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+public class KalahTest {
+
+    @Test
+    public void nothing() {
+        assertTrue(true);
+    }
+}
