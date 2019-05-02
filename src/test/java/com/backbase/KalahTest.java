2753e77c3fc65419cd29fd2b8e8d81dea7e10ce8
Ricardo MB <ladraum@gmail.com>
2019-05-01 22:27:02 -0700

    Fixed typo on test name and added test to check if the Kalah game exists

--- src/test/java/com/backbase/KalahTest.java
+++ src/test/java/com/backbase/KalahTest.java
@@ -2,12 +2,18 @@ package com.backbase;
 
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class KalahTest {
 
     @Test
-    public void assetThatTestsCanRun() {
+    public void assertThatTestsCanRun() {
         assertTrue(true);
+    }
+
+    @Test
+    public void assertThatMainGameClassExists() {
+        assertNotNull(new Kalah());
     }
 }
