ffab7ea02293cda5f211e2bd7251dde251860349
Ricardo MB <ladraum@gmail.com>
2019-05-01 23:51:24 -0700

    [GREEN] - Fixed initialization of status

--- src/main/java/com/backbase/Kalah.java
+++ src/main/java/com/backbase/Kalah.java
@@ -2,10 +2,15 @@ package com.backbase;
 
 import lombok.Data;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Data
 public class Kalah {
     private Integer gameNumber;
     private List<Integer> status;
+
+    public Kalah() {
+        setStatus(new ArrayList<>());
+    }
 }
