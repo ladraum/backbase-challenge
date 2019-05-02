1b082923dd064c12481900f16ccfa47bbb848658
Ricardo MB <ladraum@gmail.com>
2019-05-01 23:01:23 -0700

    [RED] - Added test to ensure that the game has pits on the status

--- src/test/java/com/backbase/KalahTest.java
+++ src/test/java/com/backbase/KalahTest.java
@@ -2,8 +2,12 @@ package com.backbase;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.CoreMatchers.instanceOf;
 
 public class KalahTest {
 
@@ -20,5 +24,10 @@ public class KalahTest {
     @Test
     public void assertThatGameHasAStatus() {
         assertNotNull(new Kalah().getStatus());
+    }
+
+    @Test
+    public void assertThatStatusIsAList() {
+        assertThat(new Kalah().getStatus(), instanceOf(List.class));
     }
 }
