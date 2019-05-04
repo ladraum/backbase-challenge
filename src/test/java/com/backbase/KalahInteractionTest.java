9e31f200ddb9ced77eebb33efd840ee982ecc4c4
Ricardo MB <ladraum@gmail.com>
2019-05-03 20:38:55 -0700

    [RED] Player should be able to move a single seed to the next pit

--- src/test/java/com/backbase/KalahInteractionTest.java
+++ src/test/java/com/backbase/KalahInteractionTest.java
@@ -2,6 +2,7 @@ package com.backbase;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.HashMap;
 import java.util.Map;
 
 import static org.hamcrest.CoreMatchers.instanceOf;
@@ -28,5 +29,33 @@ public class KalahInteractionTest {
     @Test
     public void assertThatPlayerCantMoveFromHouseOnFourteen() throws Exception {
         assertThrows(IllegalKalahMoveException.class, () -> {new Kalah().playMove(14);});
+    }
+
+    @Test
+    public void assertThatPlayerCanMoveASingleSeedToTheNextPit() throws Exception {
+        Map<Integer, Integer> status = new HashMap<>();
+        for (int i = 1; i <= 14; i++) {
+            status.put(i, 0);
+        }
+        status.put(3, 1);
+        Kalah game = new Kalah();
+        game.setStatus(status);
+
+        game.playMove(3);
+
+        assertEquals(status.get(1), Integer.valueOf(0));
+        assertEquals(status.get(2), Integer.valueOf(0));
+        assertEquals(status.get(3), Integer.valueOf(0));
+        assertEquals(status.get(4), Integer.valueOf(1));
+        assertEquals(status.get(5), Integer.valueOf(0));
+        assertEquals(status.get(6), Integer.valueOf(0));
+        assertEquals(status.get(7), Integer.valueOf(0));
+        assertEquals(status.get(8), Integer.valueOf(0));
+        assertEquals(status.get(9), Integer.valueOf(0));
+        assertEquals(status.get(10), Integer.valueOf(0));
+        assertEquals(status.get(11), Integer.valueOf(0));
+        assertEquals(status.get(12), Integer.valueOf(0));
+        assertEquals(status.get(13), Integer.valueOf(0));
+        assertEquals(status.get(14), Integer.valueOf(0));
     }
 }
