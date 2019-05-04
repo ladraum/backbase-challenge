9d4281c7c255a80c0089f6b0fe49da5ec53f7beb
Ricardo MB <ladraum@gmail.com>
2019-05-04 12:51:44 -0700

    Added PUT interface to call game move

--- src/main/java/com/backbase/board/BoardController.java
+++ src/main/java/com/backbase/board/BoardController.java
@@ -1,8 +1,12 @@
 package com.backbase.board;
 
+import com.backbase.game.FinishedGameException;
+import com.backbase.game.IllegalKalahMoveException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.net.InetAddress;
@@ -15,10 +19,18 @@ public class BoardController {
     @Autowired
     Environment environment;
 
-    @PostMapping("/")
+    @PostMapping("/games")
     NewGame createNewGame() {
         String serverPort = environment.getProperty("local.server.port");
         String host = InetAddress.getLoopbackAddress().getHostName();
         return board.createNewGame(host, serverPort);
+    }
+
+    @PutMapping("/games/{gameId}/pits/{pitId}")
+    GameStatus makeMove(@PathVariable Integer gameId, @PathVariable Integer pitId)
+            throws GameNotFoundException, FinishedGameException, IllegalKalahMoveException {
+        String serverPort = environment.getProperty("local.server.port");
+        String host = InetAddress.getLoopbackAddress().getHostName();
+        return board.makeMove(gameId, pitId, host, serverPort);
     }
 }
