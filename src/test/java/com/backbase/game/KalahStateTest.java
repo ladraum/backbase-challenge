bed1095a6bdba87420ec4cfb2132d39a8c561f0d
Ricardo MB <ladraum@gmail.com>
2019-05-04 03:01:25 -0700

    [RED] Game should be finished when there are no seeds on player one side

--- src/test/java/com/backbase/game/KalahStateTest.java
+++ src/test/java/com/backbase/game/KalahStateTest.java
@@ -2,12 +2,31 @@ package com.backbase.game;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.Map;
+
 import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class KalahStateTest {
 
     @Test
     public void assertThatGameShouldStartNotFinished() {
         assertFalse(new Kalah().isGameFinished());
+    }
+
+    @Test
+    public void assertThatGameIsFinishedWhenNoSeedsOnPlayerOneSide() {
+        Kalah game = new Kalah();
+        Map<Integer, Integer> status = game.getStatus();
+        status.put(0, 0);
+        status.put(1, 0);
+        status.put(2, 0);
+        status.put(3, 0);
+        status.put(4, 0);
+        status.put(5, 0);
+        status.put(6, 0);
+        game.setStatus(status);
+
+        assertTrue(game.isGameFinished());
     }
 }
