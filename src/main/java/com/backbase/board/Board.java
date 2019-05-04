c6fd5723d8d440adbbd0a2e9f80bc41d20b8302d
Ricardo MB <ladraum@gmail.com>
2019-05-04 03:54:03 -0700

    [GREEN] Board should start without games

--- src/main/java/com/backbase/board/Board.java
+++ src/main/java/com/backbase/board/Board.java
@@ -1,4 +1,12 @@
 package com.backbase.board;
 
+import com.backbase.game.Kalah;
+import lombok.Getter;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Board {
+    @Getter
+    List<Kalah> games = new ArrayList<>();
 }
