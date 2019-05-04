755131a89d47ed95ad9b635bbccdfd17fdba9189
Ricardo MB <ladraum@gmail.com>
2019-05-04 03:49:56 -0700

    [RED] Board with games should exist

---
+++ src/test/java/com/backbase/board/BoardTest.java
@@ -0,0 +1,14 @@
+package com.backbase.board;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BoardTest {
+
+    @Test
+    public void assertThatBoardExists() {
+        assertNotNull(new Board());
+    }
+
+}
